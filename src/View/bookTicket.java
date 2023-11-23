8a9dd41877e08a39fa74fb156c9eda1572b8ce6f
ASUS <ASUS@LAPTOP-BLL688S2>
2023-11-23 16:37:03 +0700

    commit bookticket payment

--- src/View/bookTicket.java
+++ src/View/bookTicket.java
@@ -7,6 +7,8 @@ package View;
 import Controller.Controller;
 import Controller.DateLabelFormatter;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Properties;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -15,6 +17,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import java.sql.Date;
+import javax.swing.JOptionPane;
 import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.SqlDateModel;
@@ -25,16 +28,16 @@ import org.jdatepicker.impl.UtilDateModel;
  * @author ASUS
  */
 public class bookTicket {
-    public bookTicket(int id){
-        bookingTicket(id);
+    public bookTicket(int id_user){
+        bookingTicket(id_user);
     }
     
     Controller con = new Controller();
     
-    private void bookingTicket(int id){
+    private void bookingTicket(int id_user){
         //=============BAGIAN CONTAINER================
         JFrame form = new JFrame("Booking Ticket Menu");
-        form.setSize(365, 360);
+        form.setSize(365, 425);
         form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         form.setLocationRelativeTo(null);
         form.setLayout(null);
@@ -52,9 +55,9 @@ public class bookTicket {
         form.add(labelCityDeparture);
         //ComboBox City Departure
         String depertureCity[] = con.getDeparture().toArray(new String [con.getDeparture().size()]);
-        JComboBox departureCity =new JComboBox(depertureCity);
-        departureCity.setBounds(150, 40, 150, 30);
-        form.add(departureCity);
+        JComboBox cbDepartureCity =new JComboBox(depertureCity);
+        cbDepartureCity.setBounds(150, 40, 150, 30);
+        form.add(cbDepartureCity);
         
         //City Arrival
         JLabel labelCityArrival = new JLabel("City Arrival :");
@@ -87,22 +90,41 @@ public class bookTicket {
         labelCompany.setBounds(20, 145, 150, 30);
         form.add(labelCompany);
         //ComboBox Airlines Company
-        
         String company[] = con.getAirline().toArray(new String [con.getAirline().size()]);
         JComboBox cbCompany =new JComboBox(company);
         cbCompany.setBounds(150, 145, 150, 30);
         form.add(cbCompany);
         
+        //Class
+        JLabel labelClass = new JLabel("Class :");
+        labelClass.setBounds(20, 180, 150, 30);
+        form.add(labelClass);
+        //ComboBox Class
+        String classPlane[] = con.getSeatClass().toArray(new String [con.getSeatClass().size()]);
+        JComboBox cbClass = new JComboBox(classPlane);
+        cbClass.setBounds(150, 180, 150, 30);
+        form.add(cbClass);
+        
+        //Seat Number
+        JLabel labelSeatClass = new JLabel("Seat :");
+        labelSeatClass.setBounds(20, 215, 150, 30);
+        form.add(labelSeatClass);
+        //ComboBox Class
+        String seatPlane[] = con.getSeatNumber().toArray(new String [con.getSeatClass().size()]);
+        JComboBox cbSeat = new JComboBox(seatPlane);
+        cbSeat.setBounds(150, 215, 150, 30);
+        form.add(cbSeat);
+        
         //Flight Preference
         JLabel labelPref = new JLabel("Flight Preference : ");
-        labelPref.setBounds(20, 175, 150, 30);
+        labelPref.setBounds(20, 250, 150, 30);
         form.add(labelPref);
         JRadioButton radioOverNight = new JRadioButton();
         radioOverNight.setText("Over Night");
-        radioOverNight.setBounds(150, 175, 100, 30);
+        radioOverNight.setBounds(150, 250, 100, 30);
         JRadioButton radioLateNight = new JRadioButton();
         radioLateNight.setText("Late Night");
-        radioLateNight.setBounds(250, 175, 100, 30);
+        radioLateNight.setBounds(250, 250, 100, 30);
         form.add(radioOverNight);
         form.add(radioLateNight);
         //Group Button
@@ -112,14 +134,14 @@ public class bookTicket {
         
         //FnB Choose
         JLabel labelFnB = new JLabel("Food : ");
-        labelFnB.setBounds(20, 210, 50, 30);
+        labelFnB.setBounds(20, 285, 50, 30);
         form.add(labelFnB);
         JRadioButton radioIndoFood = new JRadioButton();
         radioIndoFood.setText("Indonesian Food");
-        radioIndoFood.setBounds(70, 210, 130, 30);
+        radioIndoFood.setBounds(70, 285, 130, 30);
         JRadioButton radioWestFood = new JRadioButton();
         radioWestFood.setText("Western Food");
-        radioWestFood.setBounds(200, 210, 150, 30);
+        radioWestFood.setBounds(200, 285, 150, 30);
         form.add(radioIndoFood);
         form.add(radioWestFood);
         //Group Button
@@ -130,14 +152,40 @@ public class bookTicket {
         JLabel captionFood = new JLabel("Free Indonesia Food, Western Food +Rp.20000,-");
         Font fontCaptionFood = new Font("Arial", Font.PLAIN, 9);
         captionFood.setFont(fontCaptionFood);
-        captionFood.setBounds(20, 245, 250, 10);
+        captionFood.setBounds(20, 320, 250, 10);
         form.add(captionFood);
         
         JButton buttonPayment = new JButton("Go to Payment");
-        buttonPayment.setBounds(100, 265, 150, 30);
+        buttonPayment.setBounds(100, 340, 150, 30);
         form.add(buttonPayment);
         
-        //ActionListener
+        buttonPayment.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String departureValue = (String) cbDepartureCity.getSelectedItem();
+                String arrivalValue = (String) cbArrivalCity.getSelectedItem();
+                java.util.Date dateFlight = (java.util.Date) datePicker.getModel().getValue();
+                String airlinesValue = (String) cbCompany.getSelectedItem();
+                String classValue = (String) cbClass.getSelectedItem();
+                String seatValue = (String) cbSeat.getSelectedItem();
+                String prefValue = radioOverNight.isSelected() ? "Over Night" : "Late Night";
+                String FnBValue = radioIndoFood.isSelected() ? "Indonesian Food" : "Western Food";
+                String name = con.getUsernameMember(id_user);
+                int ticketPrice = con.getSeatPrice(classValue);
+                    if("Western Food".equals(FnBValue)){
+                        ticketPrice += 20000;
+                    }
+                int promo = 0;
+                
+                boolean found = true;
+                if (found) {
+                    form.dispose();
+                    new Payment(id_user, name, departureValue, arrivalValue, dateFlight, airlinesValue, classValue, seatValue, prefValue, FnBValue, ticketPrice, promo);
+                } else {
+                    JOptionPane.showMessageDialog(form, "User Not Found", "WARNING", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
         
         form.setVisible(true);
     }
