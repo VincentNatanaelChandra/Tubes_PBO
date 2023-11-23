46e8dec98086242fd946a8c23280651500208420
Vincent <Vincent@LAPTOP-4MG5F86O>
2023-11-23 16:21:33 +0700

    Seat

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -184,4 +184,98 @@ public class Controller {
         }
         return exists;
     }
+    
+    public boolean getDestination(int destination_id) {
+        conn.connect();
+        String query = "SELECT * FROM destination WHERE destination_id = '" + destination_id + "'";
+        boolean exists = false;
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                exists = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return exists;
+    }
+    
+    public boolean getPromo(int promo_id) {
+        conn.connect();
+        String query = "SELECT * FROM promo WHERE promo_id = '" + promo_id + "'";
+        boolean exists = false;
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                exists = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return exists;
+    }
+    
+    public boolean updateFlight(int flightInput, int flight_id, ArrayList<Destination> listDestination,String flight_planeCode, String flight_company, String flight_type, String flight_planeType, int flight_totalSeat){
+        conn.connect();
+        String query = "UPDATE flight"
+                + " SET flight_id='" + flight_id + "',"
+                + "destination_id='"+ listDestination + "',"
+                + "flight_planeCode='"+ flight_planeCode + "',"
+                + "flight_company'"+ flight_company + "',"
+                + "flight_type='"+ flight_type + "',"
+                + "flight_planeType='"+ flight_planeType + "',"
+                + "flight_totalSeat='"+ flight_totalSeat + "',"
+                + "WHERE flight_id = " + flightInput;
+        PreparedStatement stm;
+        try {
+            stm = conn.con.prepareStatement(query);
+            stm.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }    
+    
+    public boolean updateDestination(int destinationInput, int destination_id, String destination_departure,String destination_arrival, Date destination_departureDate){
+        conn.connect();
+        String query = "UPDATE destination"
+                + " SET destination_id='" + destination_id + "',"
+                + "destination_departure='"+ destination_departure + "',"
+                + "destination_arrival='"+ destination_arrival + "',"
+                + "destination_departureDate='"+ destination_departureDate + "',"
+                + "WHERE destination_id = " + destinationInput;
+        PreparedStatement stm;
+        try {
+            stm = conn.con.prepareStatement(query);
+            stm.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    } 
+    
+    public boolean updatePromo(int promoInput, int promo_id, String promo_code,String promo_type, Date promo_expiredDate){
+        conn.connect();
+        String query = "UPDATE promo"
+                + " SET promo_id='" + promo_id + "',"
+                + "promo_code='"+ promo_code + "',"
+                + "promo_type='"+ promo_type + "',"
+                + "promo_expiredDate='"+ promo_expiredDate + "',"
+                + "WHERE promo_id = " + promoInput;
+        PreparedStatement stm;
+        try {
+            stm = conn.con.prepareStatement(query);
+            stm.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
