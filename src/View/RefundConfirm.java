6f7df233ddf1e177d97b6f382feb89fc5e14c7e0
filsteven11 <filsteven11@gmail.com>
2023-11-24 19:17:26 +0700

    tombol back menu confirm

--- src/View/RefundConfirm.java
+++ src/View/RefundConfirm.java
@@ -24,13 +24,13 @@ public class RefundConfirm {
 
         // Data untuk tabel (Simulasi data)
         String[][] data = {
-                {"John Doe", "12345", "Pending"},
-                {"Jane Smith", "54321", "Pending"},
+                {"John Doe", "12345","Ditinggal Nikah", "Pending"},
+                {"Jane Smith", "54321", "Telat Bangun","Pending"},
                 // Data lainnya...
         };
 
         // Nama kolom untuk tabel
-        String[] columns = {"User", "Ticket ID", "Status"};
+        String[] columns = {"User", "Ticket ID","Alasan ", "Status,"};
 
         // Model tabel
         DefaultTableModel model = new DefaultTableModel(data, columns);
@@ -56,7 +56,7 @@ public class RefundConfirm {
                 if (selectedRow != -1) {
                     String userID = (String) model.getValueAt(selectedRow, 1); // Ambil nilai ID user dari tabel
                     if (!userStatusMap.containsKey(userID) || !userStatusMap.get(userID)) {
-                        model.setValueAt("Approved", selectedRow, 2);
+                        model.setValueAt("Approved", selectedRow, 3);
                         userStatusMap.put(userID, true); // Menandai bahwa user ID ini sudah diubah statusnya
                     } else {
                         // Jika sudah diubah, tampilkan pesan bahwa user ID ini sudah memiliki status yang diubah
@@ -74,7 +74,7 @@ public class RefundConfirm {
                 if (selectedRow != -1) {
                     String userID = (String) model.getValueAt(selectedRow, 1); // Ambil nilai ID user dari tabel
                     if (!userStatusMap.containsKey(userID) || !userStatusMap.get(userID)) {
-                        model.setValueAt("Rejected", selectedRow, 2);
+                        model.setValueAt("Rejected", selectedRow, 3);
                         userStatusMap.put(userID, true); // Menandai bahwa user ID ini sudah diubah statusnya
                     } else {
                         // Jika sudah diubah, tampilkan pesan bahwa user ID ini sudah memiliki status yang diubah
