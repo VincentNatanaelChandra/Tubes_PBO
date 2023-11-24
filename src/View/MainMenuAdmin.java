5171931ddb9779a0e2df064fe8fc30ebe69d58fb
filsteven11 <filsteven11@gmail.com>
2023-11-24 15:11:40 +0700

    menu user

--- src/View/MainMenuAdmin.java
+++ src/View/MainMenuAdmin.java
@@ -4,16 +4,27 @@
  */
 package View;
 
+import java.awt.Desktop;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
 /**
  *
  * @author ASUS
  */
 public class MainMenuAdmin {
+   private JFrame frame;
+
+    
+    
     public MainMenuAdmin(int id,String nama){
         MenuAdmin(id, nama);
     }
@@ -21,7 +32,7 @@ public class MainMenuAdmin {
     private void MenuAdmin(int id, String nama){
         //=============BAGIAN CONTAINER================
         JFrame formMenuAdmin = new JFrame("Menu Admin");
-        formMenuAdmin.setSize(320, 300);
+        formMenuAdmin.setSize(320, 330);
         formMenuAdmin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         formMenuAdmin.setLocationRelativeTo(null);
         formMenuAdmin.setLayout(null);
@@ -33,31 +44,80 @@ public class MainMenuAdmin {
         labelTitle.setBounds(10, 5, 400, 30);
         formMenuAdmin.add(labelTitle);
         
-        JButton buttonUpdateSchedule = new JButton("Update Schedule");
-        buttonUpdateSchedule.setBounds(30, 40, 250, 30);
-        formMenuAdmin.add(buttonUpdateSchedule);
+        JButton buttonUpdateDestination = new JButton("Update Destination");
+        buttonUpdateDestination.setBounds(30, 40, 250, 30);
+        formMenuAdmin.add(buttonUpdateDestination);
+    
+         UpdateDestinasi updateDestinasi = new UpdateDestinasi();
+
+          buttonUpdateDestination.addActionListener(new ActionListener() {
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                  // Menutup tampilan saat ini (MainMenuAdmin)
+                  formMenuAdmin.setVisible(false);
+
+                  // Menampilkan tampilan baru dari UpdateDestination
+                  updateDestinasi.showUpdateDestinasiWindow(true);
+              }
+          });   
         
+        JButton buttonUpdateFlight = new JButton("Update Flight");
+        buttonUpdateFlight.setBounds(30, 75, 250, 30);
+        formMenuAdmin.add(buttonUpdateFlight);
+        
+        UpdatePenerbangan updateFlight = new UpdatePenerbangan();
+
+          buttonUpdateFlight.addActionListener(new ActionListener() {
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                  // Menutup tampilan saat ini (MainMenuAdmin)
+                  formMenuAdmin.setVisible(false);
+
+                  // Menampilkan tampilan baru dari UpdateFlight
+                  updateFlight.showUpdatePenerbanganWindow(true);
+              }
+          });
+          
         JButton buttonUpdatePromo = new JButton("Update Promo");
-        buttonUpdatePromo.setBounds(30, 75, 250, 30);
+        buttonUpdatePromo.setBounds(30, 110, 250, 30);
         formMenuAdmin.add(buttonUpdatePromo);
-        
+
+         
+          UpdatePromo updatePromo = new UpdatePromo();
+
+          buttonUpdatePromo.addActionListener(new ActionListener() {
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                  // Menutup tampilan saat ini (MainMenuAdmin)
+                  formMenuAdmin.setVisible(false);
+
+                  // Menampilkan tampilan baru dari UpdatePromo
+                  updatePromo.showUpdatePromoWindow(true);
+              }
+          });
+
         JButton buttonConfirmationReschedule = new JButton("Reschedule Confirmation");
-        buttonConfirmationReschedule.setBounds(30, 110, 250, 30);
+        buttonConfirmationReschedule.setBounds(30, 145, 250, 30);
         formMenuAdmin.add(buttonConfirmationReschedule);
         
         JButton buttonConfirmationRefund = new JButton("Refund Confirmation");
-        buttonConfirmationRefund.setBounds(30, 145, 250, 30);
+        buttonConfirmationRefund.setBounds(30, 180, 250, 30);
         formMenuAdmin.add(buttonConfirmationRefund);
         
         JButton buttonHistory = new JButton("View History");
-        buttonHistory.setBounds(30, 180, 250, 30);
+        buttonHistory.setBounds(30, 215, 250, 30);
         formMenuAdmin.add(buttonHistory);
         
         JButton buttonBacktoLogin = new JButton("Back");
-        buttonBacktoLogin.setBounds(30, 215, 250, 30);
+        buttonBacktoLogin.setBounds(30, 250, 250, 30);
         formMenuAdmin.add(buttonBacktoLogin);
         
         formMenuAdmin.setVisible(true);
     }
+    
+    
+    public static void main(String[] args) {
+        new MainMenuAdmin(11,"tono");
+}
 }
 
--- src/View/UpdatePenerbangan.java
+++ src/View/UpdatePenerbangan.java
@@ -1,69 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package View;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class UpdatePenerbangan {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Update Flight Menu");
-            frame.setSize(300, 200);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private JFrame frame;
 
-            JPanel panel = new JPanel();
-            panel.setLayout(new GridLayout(6, 1, 10, 10));
-            panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+    public UpdatePenerbangan() {
+        createUpdatePenerbanganWindow();
+    }
 
-            JTextField field1 = new JTextField(10);
-            JTextField field2 = new JTextField(10);
-            JTextField field3 = new JTextField(10);
-            JTextField field4 = new JTextField(10);
-            JTextField field5 = new JTextField(10);
-            
-            panel.add(new JLabel("Plane Code:"));
-            panel.add(field1);
-            panel.add(new JLabel("Company:"));
-            panel.add(field2);
-            panel.add(new JLabel("Type:"));
-            panel.add(field3);
-             panel.add(new JLabel("Plane Type:"));
-            panel.add(field4);
-             panel.add(new JLabel("Total Seat:"));
-            panel.add(field5);
-             panel.add(Box.createVerticalGlue()); 
+    private void createUpdatePenerbanganWindow() {
+        frame = new JFrame("Update Flight Menu");
+        frame.setSize(300, 250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            JButton updateButton = new JButton("Update");
-            updateButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-            updateButton.addActionListener(e -> {
-                String input1 = field1.getText();
-                String input2 = field2.getText();
-                String input3 = field3.getText();
-                String input4 = field4.getText();
-                String input5 = field5.getText();
-                JOptionPane.showMessageDialog(frame, "Data has been updated", "Informasi", JOptionPane.INFORMATION_MESSAGE);
-            });
-            panel.add(updateButton);
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(6, 1, 10, 10));
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-            frame.add(panel);
-            frame.setVisible(true);
+        JTextField field1 = new JTextField(10);
+        JTextField field2 = new JTextField(10);
+        JTextField field3 = new JTextField(10);
+        JTextField field4 = new JTextField(10);
+        JTextField field5 = new JTextField(10);
+
+        panel.add(new JLabel("Plane Code:"));
+        panel.add(field1);
+        panel.add(new JLabel("Company:"));
+        panel.add(field2);
+        panel.add(new JLabel("Type:"));
+        panel.add(field3);
+        panel.add(new JLabel("Plane Type:"));
+        panel.add(field4);
+        panel.add(new JLabel("Total Seat:"));
+        panel.add(field5);
+        panel.add(Box.createVerticalGlue());
+
+        JButton updateButton = new JButton("Update");
+        updateButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        updateButton.addActionListener(e -> {
+            String input1 = field1.getText();
+            String input2 = field2.getText();
+            String input3 = field3.getText();
+            String input4 = field4.getText();
+            String input5 = field5.getText();
+            JOptionPane.showMessageDialog(frame, "Data has been updated", "Information", JOptionPane.INFORMATION_MESSAGE);
         });
-    }
-
-    // Metode untuk membuat panel dengan label dan field
-    private static JPanel createFieldPanel(String labelText, JTextField textField) {
-        JPanel fieldPanel = new JPanel();
-        fieldPanel.setLayout(new BoxLayout(fieldPanel, BoxLayout.X_AXIS));
-
-        fieldPanel.add(new JLabel(labelText));
-        fieldPanel.add(Box.createRigidArea(new Dimension(10, 0))); // Untuk spasi antara label dan field
-        fieldPanel.add(textField);
-
-        return fieldPanel;
-    }
+    panel.add(updateButton);
+    frame.add(panel);
+    
+    JButton backButton = new JButton("Back");
+    backButton.addActionListener(e -> {
+    });
+    panel.add(backButton);
+    frame.add(panel);
 }
 
+    public void showUpdatePenerbanganWindow(boolean visible) {
+        frame.setVisible(visible);
+    }
 
-
+}
