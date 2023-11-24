6678ae6b8652e3bb05f8801e9e9c347e92bb798b
steven-gns0164 <nicholassteven4@gmail.com>
2023-11-25 06:20:25 +0700

    commit tiket

--- src/View/Ticket.java
+++ src/View/Ticket.java
@@ -4,120 +4,131 @@
  */
 package View;
 
+import Controller.Controller;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author ASUS
  */
 public class Ticket {
-    public Ticket(){
-        Ticket();
+
+    public Ticket(int member_id, String name, int flight_id, String ticket_code, int ticket_id, String classSeat, String seatNum) {
+        Ticket(member_id, name, flight_id, ticket_code, ticket_id, classSeat, seatNum);
     }
-    
-    private void Ticket(){
-     //=============BAGIAN CONTAINER================
+
+    private void Ticket(int member_id, String name, int flight_id, String ticket_code, int ticket_id, String classSeat, String seatNum) {
+
+        Controller con = new Controller();
+
+        //=============BAGIAN CONTAINER================
         JFrame ticket = new JFrame("TICKET");
         ticket.setSize(300, 380);
         ticket.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ticket.setLocationRelativeTo(null);
         ticket.setLayout(null);
         //================END CONTAINER=================
-        
+
         JLabel labelTitle = new JLabel("--- Success BookTicket ---");
-        Font fontTitle = new  Font("Arial", Font.BOLD, 15);
+        Font fontTitle = new Font("Arial", Font.BOLD, 15);
         labelTitle.setFont(fontTitle);
         labelTitle.setBounds(45, 5, 500, 50);
         ticket.add(labelTitle);
-        
+
         JLabel lineTicket1 = new JLabel("_________________");
         lineTicket1.setBounds(82, 32, 150, 30);
         ticket.add(lineTicket1);
-        
+
         JLabel labelKodeTicket = new JLabel("| No. Ticket   : ");
         labelKodeTicket.setBounds(80, 45, 90, 30);
         ticket.add(labelKodeTicket);
-        
-        JLabel kodeTicket = new JLabel("9485 |");
+
+        JLabel kodeTicket = new JLabel(ticket_code + " |");
         kodeTicket.setBounds(165, 45, 80, 30);
         ticket.add(kodeTicket);
-        
+
         JLabel lineTicket2 = new JLabel("_________________");
         lineTicket2.setBounds(82, 47, 150, 30);
         ticket.add(lineTicket2);
-        
+
         JLabel labelPlaneCode = new JLabel("PLANE CODE");
         labelPlaneCode.setFont(fontTitle);
         labelPlaneCode.setBounds(10, 70, 110, 30);
         ticket.add(labelPlaneCode);
-        
-        JLabel planeCode = new JLabel("ADN-001");
+
+        String planeCode = con.getPlaneCode(flight_id);
+
+        JLabel planeCodeValue = new JLabel(planeCode);
         Font fontPlaneCode = new Font("Arial", Font.BOLD, 30);
-        planeCode.setFont(fontPlaneCode);
-        planeCode.setBounds(80, 90, 200, 50);
-        ticket.add(planeCode);
-        
+        planeCodeValue.setFont(fontPlaneCode);
+        planeCodeValue.setBounds(80, 90, 200, 50);
+        ticket.add(planeCodeValue);
+
         Font fontTitleAtribute = new Font("Arial", Font.PLAIN, 13);
         Font fontAtribute = new Font("Arial", Font.BOLD, 17);
-        int formWidth = ticket.getWidth();   
-        
+        int formWidth = ticket.getWidth();
+
         JLabel labelName = new JLabel("Name");
         labelName.setBounds(10, 130, 100, 30);
         labelName.setFont(fontTitleAtribute);
         ticket.add(labelName);
-        
-        JLabel name = new JLabel("Vincent");
-        name.setFont(fontAtribute);
-        name.setBounds(10, 150, 100, 30);
-        ticket.add(name);
-        
+
+        JLabel nameValue = new JLabel(name);
+        nameValue.setFont(fontAtribute);
+        nameValue.setBounds(10, 150, 100, 30);
+        ticket.add(nameValue);
+
         JLabel labelClass = new JLabel("Class");
         labelClass.setBounds(10, 180, 100, 30);
         labelClass.setFont(fontTitleAtribute);
         ticket.add(labelClass);
-        
-        JLabel lClass = new JLabel("Economy");
+
+        JLabel lClass = new JLabel(classSeat);
         lClass.setBounds(10, 200, 100, 30);
         lClass.setFont(fontAtribute);
         ticket.add(lClass);
-        
+
         JLabel labelId = new JLabel("ID");
         labelId.setBounds(formWidth - 42, 130, 50, 30);
         labelId.setFont(fontTitleAtribute);
         ticket.add(labelId);
-        
-        JLabel id = new JLabel("1");
-        id.setFont(fontAtribute);
-        id.setBounds(formWidth - 40, 150, 100, 30);
-        ticket.add(id);
-        
+
+        JLabel idValue = new JLabel(member_id + "");
+        idValue.setFont(fontAtribute);
+        idValue.setBounds(formWidth - 40, 150, 100, 30);
+        ticket.add(idValue);
+
         JLabel labelSeat = new JLabel("Seat");
         labelSeat.setBounds(formWidth - 55, 180, 100, 30);
         labelSeat.setFont(fontTitleAtribute);
         ticket.add(labelSeat);
-        
-        JLabel seat = new JLabel("E1");
+
+        JLabel seat = new JLabel(seatNum);
         seat.setBounds(formWidth - 50, 200, 100, 30);
         seat.setFont(fontAtribute);
         ticket.add(seat);
-        
+
         JButton buttonPrintTicket = new JButton("Print Ticket");
         buttonPrintTicket.setBounds(15, 245, 255, 30);
         ticket.add(buttonPrintTicket);
-        
+
         JButton buttonBack = new JButton("Back");
         buttonBack.setBounds(15, 285, 255, 30);
         ticket.add(buttonBack);
-        
-        
-        
+
+        buttonBack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ticket.dispose();
+                new MainMenuUser(member_id, name);
+            }
+        });
         ticket.setVisible(true);
-    }
-    
-    public static void main(String[] args) {
-        new Ticket();
     }
 }
