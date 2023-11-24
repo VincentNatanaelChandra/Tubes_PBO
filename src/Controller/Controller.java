295424f382f21604e47d1381250dcfce96c3ca8d
VincentNatanaelChandra <vincentnatanaelchandra@gmail.com>
2023-11-24 20:40:28 +0700

    update"an

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -231,28 +231,6 @@ public class Controller {
         return exists;
     }
     
-    public boolean updateFlight(int flightInput, int flight_id, ArrayList<Destination> listDestination,String flight_planeCode, String flight_company, String flight_type, String flight_planeType, int flight_totalSeat){
-        conn.connect();
-        String query = "UPDATE flight"
-                + " SET flight_id='" + flight_id + "',"
-                + "destination_id='"+ listDestination + "',"
-                + "flight_planeCode='"+ flight_planeCode + "',"
-                + "flight_company'"+ flight_company + "',"
-                + "flight_type='"+ flight_type + "',"
-                + "flight_planeType='"+ flight_planeType + "',"
-                + "flight_totalSeat='"+ flight_totalSeat + "',"
-                + "WHERE flight_id = " + flightInput;
-        PreparedStatement stm;
-        try {
-            stm = conn.con.prepareStatement(query);
-            stm.executeUpdate();
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }    
-    
     public boolean updateDestination(int destinationInput, int destination_id, String destination_departure,String destination_arrival, Date destination_departureDate){
         conn.connect();
         String query = "UPDATE destination"
@@ -271,25 +249,6 @@ public class Controller {
             return false;
         }
     } 
-    
-    public boolean updatePromo(int promoInput, int promo_id, String promo_code,String promo_type, Date promo_expiredDate){
-        conn.connect();
-        String query = "UPDATE promo"
-                + " SET promo_id='" + promo_id + "',"
-                + "promo_code='"+ promo_code + "',"
-                + "promo_type='"+ promo_type + "',"
-                + "promo_expiredDate='"+ promo_expiredDate + "',"
-                + "WHERE promo_id = " + promoInput;
-        PreparedStatement stm;
-        try {
-            stm = conn.con.prepareStatement(query);
-            stm.executeUpdate();
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
     
     public ArrayList<String> getDeparture() {
         ArrayList<String> listDeparture = new ArrayList<>();
@@ -733,5 +692,22 @@ public class Controller {
             
         }
         return id;
+    }
+    
+    public boolean getTicket(String ticket) {
+        conn.connect();
+        String query = "SELECT ticket_code FROM ticket WHERE ticket_code = '" + ticket + "'";
+        boolean exists = false;
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                exists = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            
+        }
+        return exists;
     }
 }
