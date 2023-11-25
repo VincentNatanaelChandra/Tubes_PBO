763ef1f751f7a7a1c66c7c45a9ef8d945727ae20
steven-gns0164 <nicholassteven4@gmail.com>
2023-11-25 10:50:44 +0700

    Revert "commit FOR TICKET"
    This reverts commit b77aad0b717c20687d78f5dfb2b43e0734c845ae.

--- src/View/Payment.java
+++ src/View/Payment.java
@@ -46,7 +46,7 @@ public class Payment {
     private String fnb;
     private int ticketPrice;
     private int promoDisc;
-    private int flight_id;
+    private int flightId;
     private String method;
     private int finalPrice;
     private int promo_id;
@@ -65,7 +65,7 @@ public class Payment {
         this.ticketPrice = ticketPrice;
         this.promo_id = promo_id;
         this.promoDisc = promoDisc;
-        this.flight_id = flightId;
+        this.flightId = flightId;
         Payment();
     }
 
@@ -137,7 +137,7 @@ public class Payment {
                     double totalPromo = ticketPrice * discount;
                     int promo = (int) totalPromo;
                     formPayment.dispose();
-                    new Payment(member_id, name, departureCity, arrivalCity, dateFlight, airlines, classSeat, seatNum, preference, fnb, ticketPrice, promo_id, promo, flight_id);
+                    new Payment(member_id, name, departureCity, arrivalCity, dateFlight, airlines, classSeat, seatNum, preference, fnb, ticketPrice, promo_id, promo, flightId);
                 } else {
                     JOptionPane.showMessageDialog(formPayment, "Promo Not Found!", "WARNING", JOptionPane.WARNING_MESSAGE);
                 }
@@ -250,16 +250,17 @@ public class Payment {
         Controller con = new Controller();
         
         //Generate Ticker Code
-        String ticket_code = String.valueOf((int) (Math.random() * 10000));
+        String ticketCode = String.valueOf((int) (Math.random() * 10000));
         
         LocalDate currentDate = LocalDate.now();
         java.sql.Date sqlDate = java.sql.Date.valueOf(currentDate);
         
         //Insert Data
-        con.registerTicket(flight_id, ticket_code, sqlDate, preference);
-        int ticket_id = con.getTicketIdInt(ticket_code);
+        con.registerTicket(flightId, ticketCode, sqlDate, preference);
+        int ticket_id = con.getTicketIdInt(ticketCode);
+        System.out.println(ticket_id);
         con.registerTransaction(ticket_id, method, ticketPrice, promoDisc, finalPrice, promo_id, member_id);
         
-        new Ticket(member_id, name, flight_id, ticket_code, ticket_id, classSeat, seatNum);
+        JOptionPane.showMessageDialog(formPayment, "Done", "Done ga bang?", JOptionPane.PLAIN_MESSAGE);
     }
 }
